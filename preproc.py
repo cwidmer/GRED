cb1ff58d33e9726c3563b536b8560006cf34e0d4
Christian Widmer <cwidmer@tuebingen.mpg.de>
2012-08-28 14:46:34 -0400

    removed some unneeded code

--- preproc.py
+++ preproc.py
@@ -3,12 +3,14 @@ import vigra.filters
 import data_processing
 import numpy
 
+
 def load_data():
     #readImage(filename, dtype = 'FLOAT', index = 0, order='') -> Image
     #TODO read tiff files
     vigra.impex.readImage()
 
 def extract(data=None, *varargin):
+
     # This function localizes blob-like object using multi-scale hessian
     # aggregation. The algorithm has been described in 
     # [*} Xinghua Lou, X. Lou, U. Koethe, J. Wittbrodt, and F. A. Hamprecht. 
@@ -38,7 +40,7 @@ def extract(data=None, *varargin):
     closing = True
     opening = False
     window = 3
-    thresholds = numpy.array([-0.5, -0.5, -0.5])
+    thresholds = numpy.array([-0.01, -0.1, -0.2])
     conn = 0
     margin = 0
     verbose = False
@@ -46,7 +48,7 @@ def extract(data=None, *varargin):
     sigmas = numpy.array([1, 2, 3])
 
     #seeds = arg(varargin, mstring('init'), true(size(data)))
-    seeds = data.size
+    seeds = numpy.ones(data.size)
 
     for scale in scales:
 
@@ -61,11 +63,12 @@ def extract(data=None, *varargin):
 
 
         # smooth image at this scale
-        tmp = vigra.filters.gaussianSmoothing(data, 0.4)
+        tmp = vigra.filters.gaussianSmoothing(data, scale)
 
         # compute eigenvalues
         #eigenValues = vigra.filters.eigenValueOfHessianMatrix(tmp, sigma, 0.9 * numpy.array([1, 1, 1]), mask, seeds)
-        hessian = vigra.filters.hessianOfGaussianEigenvalues(data, scale) #, out=None, sigma_d=0.0, step_size=1.0, window_size=0.0, roi=None)
+        tmp_sigma = numpy.eye(3)
+        hessian = vigra.filters.hessianOfGaussianEigenvalues(data, tmp_sigma) #, out=None, sigma_d=0.0, step_size=1.0, window_size=0.0, roi=None)
         import ipdb
         ipdb.set_trace()
 
@@ -80,62 +83,12 @@ def extract(data=None, *varargin):
         end
     end
 
-    # morphological operations
-    if margin > 0:    # remove seeds at the boundaries/surfaces
-        struc = false(size(seeds))
-        if ndims(data) == 3:
-            struc(mslice[margin + 1:end - margin], mslice[margin + 1:end - margin], mslice[margin + 1:end - margin]).lvalue = true
-        elif ndims(data) == 2:
-            struc(mslice[margin + 1:end - margin], mslice[margin + 1:end - margin]).lvalue = true
-        end
-        seeds = logical_and(seeds, struc)
-    end
+    #igra.filters.discClosing()
+    #http//hci.iwr.uni-heidelberg.de/vigra/doc/vigranumpy/index.html?highlight=dilate
 
-    if numel(closing) == 1:    # connect proximate seeds
-        if closing:
-            if ndims(data) == 3:
-                seeds = imclose(seeds, true(1, 1, window))
-                seeds = imclose(seeds, true(1, window, 1))
-                seeds = imclose(seeds, true(window, 1, 1))
-            elif ndims(data) == 2:
-                seeds = imclose(seeds, true(1, window))
-                seeds = imclose(seeds, true(window, 1))
-                tmp = diag(true(window, 1))
-                seeds = imclose(seeds, tmp)
-                seeds = imclose(seeds, tmp(mslice[:], mslice[end:-1:1]))
-            end
-        end
-    else:
-        seeds = imclose(seeds, closing)
-    end
-
-    if numel(opening) == 1:    # remove noisy seeds
-        if opening:
-            if ndims(data) == 3:
-                struc = false(window, window, window)
-                c = (window + 1) / 2
-                struc(mslice[:], c, c).lvalue = true
-                struc(c, mslice[:], c).lvalue = true; print struc
-                struc(c, c, mslice[:]).lvalue = true
-
-                seeds = imopen(seeds, struc)
-            elif ndims(data) == 2:
-                struc = false(window, window)
-                c = (window + 1) / 2
-                struc(mslice[:], c).lvalue = true
-                struc(c, mslice[:]).lvalue = true; print struc
-
-                seeds = imopen(seeds, struc)
-            end
-        end
-    else:
-        seeds = imopen(seeds, opening)
-    end
-
-    if conn != 0:
-        seeds = ctConnectedComponentAnalysis(uint16(seeds), false)
-    end
-    seeds = uint16(seeds)
+    #vigra.analysis.labelVolume()
+    #vigra.analysis.labelImage()
 
 if __name__ == "__main__":
     extract()
+
